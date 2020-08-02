4863f4952fd82788423f92dda1d8e59fd1cfed72
Mohamed Elleuch <65103380+ElleuchX1@users.noreply.github.com>
2020-08-02 17:27:11 +0200

    Create TCP_Client.py

---
+++ TCP_Client.py
@@ -0,0 +1,9 @@
+import socket
+C_socket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+
+port=1111
+C_socket.connect(('yourhost',port))
+mess= C_socket.recv(1024) #maxdata 1024
+C_socket.close()
+print(mess.decode('ascii'))
